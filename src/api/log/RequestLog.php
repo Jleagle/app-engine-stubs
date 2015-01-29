93a08f49e567bfa0b2fe7c07d5bfc56a223d9a67
Jleagle <jimeagle@gmail.com>
2015-01-29 20:49:55 +0000

    Log

--- src/api/log/RequestLog.php
+++ src/api/log/RequestLog.php
@@ -1,8 +1,222 @@
 <?php
-// todo
 namespace google\appengine\api\log\AppLogLine;
 
 class RequestLog
 {
+  /**
+   * @return string
+   */
+  public function getAppEngineRelease()
+  {
+  }
 
+  /**
+   * @return string
+   */
+  public function getAppId()
+  {
+  }
+
+  /**
+   * @return AppLogLine[]
+   */
+  public function getAppLogs()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getCombined()
+  {
+  }
+
+  /**
+   * @return \DateTime
+   */
+  public function getEndDateTime()
+  {
+  }
+
+  /**
+   * @return double
+   */
+  public function getEndTimeUsec()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getHost()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getHttpVersion()
+  {
+  }
+
+  /**
+   * @return int
+   */
+  public function getInstanceIndex()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getInstanceKey()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getIp()
+  {
+  }
+
+  /**
+   * @return double
+   */
+  public function getLatencyUsec()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getMethod()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getModuleId()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getNickname()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getOffset()
+  {
+  }
+
+  /**
+   * @return double
+   */
+  public function getPendingTimeUsec()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getReferrer()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getRequestId()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getResource()
+  {
+  }
+
+  /**
+   * @return int
+   */
+  public function getResponseSize()
+  {
+  }
+
+  /**
+   * @return \DateTime
+   */
+  public function getStartDateTime()
+  {
+  }
+
+  /**
+   * @return double
+   */
+  public function getStartTimeUsec()
+  {
+  }
+
+  /**
+   * @return int
+   */
+  public function getStatus()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getTaskName()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getTaskQueueName()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getUrlMapEntry()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getUserAgent()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function getVersionId()
+  {
+  }
+
+  /**
+   * @return bool
+   */
+  public function isFinished()
+  {
+  }
+
+  /**
+   * @return bool
+   */
+  public function isLoadingRequest()
+  {
+  }
 }
