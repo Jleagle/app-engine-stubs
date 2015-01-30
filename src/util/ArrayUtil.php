b1a8defb43c757394c09442926f42f1bf55f5003
Jleagle <jimeagle@gmail.com>
2015-01-30 19:33:18 +0000

    App identity

---
+++ src/util/ArrayUtil.php
@@ -0,0 +1,15 @@
+<?php
+namespace google\appengine\util;
+
+class ArrayUtil
+{
+  /**
+   * @param array $array
+   * @param mixed $key
+   *
+   * @return mixed
+   */
+  public static function findByKeyOrNull(array $array, $key)
+  {
+  }
+}
---
+++ src/util/StringUtil.php
@@ -0,0 +1,43 @@
+<?php
+namespace google\appengine\util;
+
+class StringUtil
+{
+  /**
+   * @param string $input
+   *
+   * @return string
+   */
+  public static function base64UrlDecode($input)
+  {
+  }
+
+  /**
+   * @param string $input
+   *
+   * @return string
+   */
+  public static function base64UrlEncode($input)
+  {
+  }
+
+  /**
+   * @param string $input
+   * @param string $suffix
+   *
+   * @return bool
+   */
+  public static function endsWith($input, $suffix)
+  {
+  }
+
+  /**
+   * @param string $input
+   * @param string $prefix
+   *
+   * @return bool
+   */
+  public static function startsWith($input, $prefix)
+  {
+  }
+}
